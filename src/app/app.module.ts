0542a86cab7227ca8e049208121cafb5d2f52d1f
DenriUs <den.tacij@gmail.com>
2020-11-19 13:10:13 +0200

    Change db location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,16 @@ import BusModule from 'src/bus/bus.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ["dist/entity/**/*.js"],
+      synchronize: true
+    }),
     AuthModule,
     UserModule,
     TripModule,
